98d1c46c0cb1d3119491cee81c65c5491ea1080d
Michal Sytko <michael.sytko@gmail.com>
2018-07-03 22:13:18 +0200

    Added ImageSpriteLoader

---
+++ OpenRA.Mods.Fantasy/SpriteLoaders/ImageSpriteLoader.cs
@@ -0,0 +1,17 @@
+﻿using OpenRA.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OpenRA.Mods.Fantasy.SpriteLoaders
+{
+	public class ImageSpriteLoader : ISpriteLoader
+	{
+		public bool TryParseSprite(Stream s, out ISpriteFrame[] frames)
+		{
+			frames = new ISpriteFrame[0];
+			return false;
+		}
+	}
+}
